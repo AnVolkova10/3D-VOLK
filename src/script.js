9a1a0a34165e2ba4468c22c142623d9aba1c89a4
AnVolkova10 <curzi.angela@gmail.com>
2022-08-10 10:11:58 -0300

    added types of light and plane object

--- src/script.js
+++ src/script.js
@@ -16,7 +16,7 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
+// OBJ1 ////////////////////////////////////////////////////
 const geometry = new THREE.SphereBufferGeometry( .5, 64, 64 );
 
 // Materials
@@ -31,14 +31,45 @@ material.color = new THREE.Color(0x292929)
 const sphere = new THREE.Mesh(geometry,material)
 scene.add(sphere)
 
+// OBJ2 //////////////////////////////////////////////
+
+const geometry2 = new THREE.PlaneBufferGeometry(3,3,64,64)
+const material2 = new THREE.MeshStandardMaterial()
+material2.metalness = 1
+material2.roughness = 1
+material2.color = new THREE.Color(0xffffff)
+const plane = new THREE.Mesh(geometry2,material2)
+scene.add(plane)
+
+plane.rotation.x = 181
+
+const planeGUI = gui.addFolder('Plane');
+planeGUI.add(plane, 'visible')
+planeGUI.add(plane.rotation, 'x').min(-3).max(3).step(0.00001)
+planeGUI.add(plane.rotation, 'y').min(-3).max(3).step(0.00001)
+planeGUI.add(plane.position, 'x').min(-3).max(3).step(0.00001)
+planeGUI.add(plane.position, 'y').min(-3).max(3).step(0.00001)
+
 // Lights
 
 //LIGHT 1
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
+const pointLight = new THREE.AmbientLight(0xffffff, 0.1)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+
+const light = gui.addFolder('Light 1 - AmbientLight')
+
+light.add(pointLight, 'visible')
+light.add(pointLight, 'castShadow')
+light.add(pointLight.position, 'y').min(-3).max(3).step(0.01)
+light.add(pointLight.position, 'x').min(-6).max(10).step(0.01)
+light.add(pointLight.position, 'z').min(-3).max(3).step(0.01)
+light.add(pointLight, 'intensity').min(0).max(10).step(0.01)
+
+const pointLightHelper0 = new THREE.PointLightHelper(pointLight, 1)
+scene.add(pointLightHelper0)
 
 // LIGHT 2 - RED
 const pointLight2 = new THREE.PointLight(0xff0000, 2)
@@ -46,40 +77,47 @@ pointLight2.position.set(1,1,1)
 pointLight2.intensity = 1
 scene.add(pointLight2)
 
-const light2 = gui.addFolder('Light 2')
+const light2 = gui.addFolder('Light 2 - PointLight')
 
+light2.add(pointLight2, 'visible')
+light2.add(pointLight2, 'castShadow')
 light2.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
 light2.add(pointLight2.position, 'x').min(-6).max(10).step(0.01)
 light2.add(pointLight2.position, 'z').min(-3).max(3).step(0.01)
 light2.add(pointLight2, 'intensity').min(0).max(10).step(0.01)
 
-const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
+const pointLightHelper = new THREE.AmbientLightProbe(pointLight2, 1)
 scene.add(pointLightHelper)
 
 // LIGHT 3 - BLUE
-const pointLight3 = new THREE.PointLight(0x0000ff, 2)
+const pointLight3 = new THREE.HemisphereLight(0xff0000,0x00ff00, 2)
 pointLight3.position.set(-3,1,1)
 pointLight3.intensity = 1
 scene.add(pointLight3)
 
-const light3 = gui.addFolder('Light 3')
+const light3 = gui.addFolder('Light 3 - HemisphereLight')
 
+light3.add(pointLight3, 'visible')
+light3.add(pointLight3, 'castShadow')
 light3.add(pointLight3.position, 'y').min(-3).max(3).step(0.01)
 light3.add(pointLight3.position, 'x').min(-6).max(10).step(0.01)
 light3.add(pointLight3.position, 'z').min(-3).max(3).step(0.01)
 light3.add(pointLight3, 'intensity').min(0).max(10).step(0.01)
 
-const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 1)
+const pointLightHelper2 = new THREE.HemisphereLightHelper(pointLight3, 1)
 scene.add(pointLightHelper2)
 
 // LIGHT 4 
-const pointLight4 = new THREE.PointLight(0xe600ad, 2)
+
+const pointLight4 = new THREE.RectAreaLight(0xe600ad, 2,2 ,20)
 pointLight4.position.set(-0.11,-3,-2.11)
 pointLight4.intensity = 4.9
 scene.add(pointLight4)
 
-const light4 = gui.addFolder('Light 4')
+const light4 = gui.addFolder('Light 4 - ReactAreaLight')
 
+light3.add(pointLight4, 'visible')
+light3.add(pointLight4, 'castShadow')
 light4.add(pointLight4.position, 'y').min(-3).max(3).step(0.01)
 light4.add(pointLight4.position, 'x').min(-6).max(10).step(0.01)
 light4.add(pointLight4.position, 'z').min(-3).max(3).step(0.01)
